439262bc587f9ababc8b6c5e8a0c07f052bbcc44
benlazzero <85390423+benlazzero@users.noreply.github.com>
2022-05-31 13:17:37 -0700

    getting frontend started

---
+++ frontend/src/login/pages/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Login.css';
+
+const Login = () => {
+  return (
+    <div className="login-wrapper">
+      <p>welcome to</p>
+      <h1>ANICHAR<br />TRIVIA</h1>
+      {/* add button components */}
+      <span>OR</span>
+      {/* add button components */}
+    </div>
+  )
+};
+
+export default Login;
