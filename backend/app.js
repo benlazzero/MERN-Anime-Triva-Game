cb8acfbd6864039a7b6dfd859f588724aff206da
benlazzero <85390423+benlazzero@users.noreply.github.com>
2022-06-02 20:18:32 -0700

    connecting front/back

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,18 @@
 // https://animechan.vercel.app/ <- this is the api
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const loginRoute = require('./routes/login-route');
 
 const app = express();
 
-
+app.use(cors());
 app.use(loginRoute);
 
-
-app.listen(3000, (error) => {
+app.listen(4000, (error) => {
   if(!error) {
-    console.log(`server running on port 3000`);
+    console.log(`server running on port 4000`);
   } else {
     console.log("error occured starting server");
   }
--- frontend/src/dashboard/components/Stats.js
+++ frontend/src/dashboard/components/Stats.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import './Stats.css';
+import TestFetch from '../../shared/components/TestFetch';
 
 const Stats = () => {
   return (
     <div className="stats-wrapper">
       <h1>Your Stats</h1>
       <span>Login to view stats</span>
+      <TestFetch />
     </div>
   );
 };
